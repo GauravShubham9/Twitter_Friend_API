e1fe394f467c4c31e4f95eadfa87bc4acbdefb5e
Saurabh Shubham <saurabh.friday@gmail.com>
2020-10-10 10:36:39 +0530

    Added the auth request

--- twurl.py
+++ twurl.py
@@ -7,3 +7,5 @@ def augment(url, parameters):
     secrets = hidden.oauth()
     consumer = oauth.OAuthConsumer(secrets['consumer_key'], secrets['consumer_secret'])
     token = oauth.OAuthToken(secrets['token_key'], secrets['token_secret'])
+    
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, token=token, http_method='GET', http_url=url, parameters=parameters)
